5b6b8684b290484078e76b1c01cea25fc86ecec9
Michael Kanzieper <125393245+mk590901@users.noreply.github.com>
2024-04-26 14:15:57 +0300

    Update PageFragment.java

--- app/src/main/java/com/widget/listenersapp/PageFragment.java
+++ app/src/main/java/com/widget/listenersapp/PageFragment.java
@@ -8,24 +8,14 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import java.util.UUID;
-
-
 public class PageFragment extends Fragment {
-
     final String TAG = PageFragment.this.getClass().getSimpleName();
     private String mName;
-    private String mUuid = UUID.randomUUID().toString();
-    private CustomTextView mTextValue;
-
-    @Nullable
-    private MainActivity mActivity;
 
     public PageFragment() {
         // Required empty public constructor
@@ -45,18 +35,11 @@ public class PageFragment extends Fragment {
 
         //  Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_page, container, false);
-        setControls(view);
         return view;
     }
 
-    private void setControls(final View view) {
-        mTextValue = view.findViewById(R.id.value1);
-        ImageView imageView = view.findViewById(R.id.image_view);
-        imageView.setImageResource(R.drawable.pinch_zoom_out);
-    }
-
     @Override
-    public void onAttach(final Context context) {
+    public void onAttach(@NonNull final Context context) {
         super.onAttach(context);
         onAttached(context);
     }
